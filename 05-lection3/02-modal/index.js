bb2246b86dff5f8f961d0ffabfb933955cfb220a
a.martyshev <a.martyshev@rambler-co.ru>
2021-01-27 22:03:21 +0300

    05-lection3 add tooltip

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,26 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function () {
+  const close = document.getElementById("modal-close");
+  const modal = document.getElementById("modal");
+  const modalOpen = document.getElementById("modal-open");
+  const fade = document.getElementById("modal-fade");
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+  if (!close || !modalOpen || !modal || !fade) {
+    return;
+  }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+  const openModal = () => {
+    modal.style.display = "flex";
+    document.body.overflow = "hidden";
+  };
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
+  const closeModal = () => {
+    modal.style.display = "none";
+    document.body.overflow = "initial";
+  };
+
+  modalOpen.addEventListener("click", openModal);
+
+  close.addEventListener("click", closeModal);
+
+  fade.addEventListener("click", closeModal);
 })();
